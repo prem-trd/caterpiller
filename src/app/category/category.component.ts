7ed7f8f08f2578f76cca0b7c566f479597e7a7b3
prem <kodamanchillipremchand@gmail.com>
2020-01-10 18:45:10 +0530

    all

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as Highcharts from 'highcharts';
 
 @Component({
   selector: 'app-category',
@@ -10,6 +11,50 @@ export class CategoryComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.pieCharts();
   }
-
+  pieCharts() {
+    var bar = Highcharts.chart('pie',{
+       chart: {
+          plotBorderWidth: null,
+          plotShadow: false
+       },
+       title: {
+          text:'Top 10 Supplier By Spend'
+       },
+       tooltip: {
+          pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+       },
+       plotOptions: {
+          pie: {
+             allowPointSelect: true,
+             cursor: 'pointer',
+             dataLabels: {
+                enabled: false,
+             },
+             showInLegend:true
+          }
+       },
+       credits: {
+        enabled: false
+     },
+          series: [{
+          type: 'pie',
+          name: 'Browser share',
+          data: [
+             ['Firefox', 45.0],
+             ['IE', 26.8],
+             {
+                name: 'Chrome',
+                y: 12.8,
+                sliced: true,
+                selected: true
+             },
+             ['Safari', 8.5],
+             ['Opera', 6.2],
+             ['Others', 32.7]
+          ]
+       }]
+    });
+ }
 }
